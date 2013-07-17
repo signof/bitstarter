3d160a2f452fbe8e5774855423eed781333ce4cc
signof <subsubkanzler@gmail.com>
2013-07-17 15:15:11 +0000

    server /css

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/css'));
 app.get('/', function(request, response) {
 	response.send(fs.readFileSync("index.html").toString());
 });
